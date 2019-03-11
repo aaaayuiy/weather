53e6e30fededcb8f1f074522010464f4b8a527a1
Jiaxin Li <jiaxinli@JiaxindeMBP.wifi.uwrf.edu>
2019-03-11 18:25:17 -0500

    Fixed Indentation.

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView, ImageBackground } from 'react-native';
+import { StyleSheet,
+  Text,
+  View,
+  Platform,
+  TextInput,
+  KeyboardAvoidingView,
+  ImageBackground } from 'react-native';
 import SearchInput from './components/SearchInput';
 import getImageForWeather from './utils/getImageForWeather';
 
