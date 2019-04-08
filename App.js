611990fcff15998a8febb3b03f0f1be6184bc630
Jiaxin Li <jiaxin.li1992@gmail.com>
2019-04-08 07:52:38 -0500

    added loading indicators and its' functionlity

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   Text,
@@ -8,12 +8,12 @@ import {
   ImageBackground,
   ActivityIndicator,
   StatusBar
-} from 'react-native';
+} from "react-native";
 
-import SearchInput from './components/SearchInput';
+import SearchInput from "./components/SearchInput";
 
-import getImageForWeather from './utils/getImageForWeather';
-import { fetchLocationId, fetchWeather } from './utils/api';
+import getImageForWeather from "./utils/getImageForWeather";
+import { fetchLocationId, fetchWeather } from "./utils/api";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -22,24 +22,24 @@ export default class App extends React.Component {
     this.state = {
       loading: false,
       error: false,
-      location: '',
+      location: "",
       temperature: 0,
-      weather: '',
+      weather: ""
     };
   }
 
   componentDidMount() {
-    this.handleUpdateLocation('San Francisco');
+    this.handleUpdateLocation("San Francisco");
   }
 
-  handleUpdateLocation = async (city) => {
+  handleUpdateLocation = async city => {
     if (!city) return;
 
     this.setState({ loading: true }, async () => {
       try {
         const locationId = await fetchLocationId(city);
         const { location, weather, temperature } = await fetchWeather(
-          locationId,
+          locationId
         );
 
         this.setState({
@@ -47,44 +47,59 @@ export default class App extends React.Component {
           error: false,
           location,
           weather,
-          temperature,
+          temperature
         });
       } catch (e) {
         this.setState({
           loading: false,
-          error: true,
+          error: true
         });
       }
     });
   };
 
   render() {
-    const { location } = this.state;
+    const { loading, error, location, weather, temperature } = this.state;
 
     return (
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior="padding"
-      >
-
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
         <ImageBackground
-          source={getImageForWeather('Light Cloud')}
+          source={getImageForWeather(weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}
         >
-
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>
-              {location}
-            </Text>
-            <Text style={[styles.smallText, styles.textStyle]}>
-              Light Cloud
-            </Text>
-            <Text style={[styles.largeText, styles.textStyle]}>25°</Text>
-            <SearchInput
-              placeholder="Search any city"
-              onSubmit={this.handleUpdateLocation}
-            />
+            <ActivityIndicator animating={loading} color="white" size="large" />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city.
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
@@ -95,52 +110,52 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#34495E',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#34495E",
+    alignItems: "center",
+    justifyContent: "center"
   },
   textStyle: {
-    textAlign: 'center',
+    textAlign: "center",
     ...Platform.select({
       ios: {
-        fontFamily: 'AvenirNext-Regular',
+        fontFamily: "AvenirNext-Regular"
       },
       android: {
-        fontFamily: 'Roboto',
-      },
+        fontFamily: "Roboto"
+      }
     }),
-    color: 'white',
+    color: "white"
   },
   largeText: {
-    fontSize: 44,
+    fontSize: 44
   },
   smallText: {
-    fontSize: 18,
+    fontSize: 18
   },
   textInput: {
-    backgroundColor: '#666',
-    color: 'white',
+    backgroundColor: "#666",
+    color: "white",
     height: 40,
     width: 300,
     marginTop: 20,
     marginHorizontal: 20,
     paddingHorizontal: 10,
-    alignSelf: 'center',
+    alignSelf: "center"
   },
   imageContainer: {
-    flex: 1,
+    flex: 1
   },
   image: {
     flex: 1,
     width: null,
     height: null,
-    resizeMode: 'cover',
+    resizeMode: "cover"
   },
   detailsContainer: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.2)',
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.2)",
     paddingHorizontal: 20,
-    alignItems: 'center',
-  },
+    alignItems: "center"
+  }
 });
